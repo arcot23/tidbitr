bab26d2cea1dfb2fb0ab061c15c92526c7960dd2
Prabhuram Venkatesan <prabhuram.v@gmail.com>
2017-04-27 07:17:19 -0400

    odbc and ora functions moved to deebee package

--- R/df_compare.R
+++ R/df_compare.R
@@ -68,21 +68,4 @@ CompareAndShowAll <- function(x, y, col.names = c("lhs_matches", "rhs_matches"))
   function (x, y)
     CompareAndShowAll(x, y)
 
-#' @title Compares two SQL resultsets from two database environments
-#' @description Takes a SQL statement as an input and executes the SQL in two database environments, then compares the SQL resultset for differences.
-#'
-#' @param query SQL Query.
-#' @param env1 Environment string to the first database.
-#' @param env2 Environment string to the second database.
-#' @param inXbutNotY If True (default) informs to find differences in X and not in Y. If False infors to find differences in Y and not in X.
-#' @return Returns a tibble of differences
-#'
-CompareOraDataset <-
-  function(query,
-           env1,
-           env2,
-           inXbutNotY = T) {
-    Compare(ora_run(query, env1),
-            ora_run(query, env2),
-            inXbutNotY)
-  }
+
