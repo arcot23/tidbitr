c7a80afe9f2a861068806dac02a4a52c01ffe5e1
Prabhuram Venkatesan <prabhuram.v@gmail.com>
2017-05-02 22:47:50 -0400

    updated   FreqTable

--- R/df_freqsummary.R
+++ R/df_freqsummary.R
@@ -4,24 +4,31 @@
 #'
 #' @param x Dataset to be processed.
 #' @param records Number of rows to show in the resulting table
+#' @param as return as "l" list or "d" dataset
 #'
 #' @return Returns a frequency table in a key/value pair format for all the columns. Number of columns returned is length of the dataset x 2.
 #'
 #' @example
 #' FreqTable(census)
-FreqTable <- function(x, records = 10) {
-  FreqSummary <- function(x, records = 10) {
-    x %>%
-      lapply(
-        FUN = function(x) {
-          as_data_frame(head(sort(table(x), decreasing = T), records))
-        }
-      )
-  }
-  len <- length(x)*2
-  x %>%
-    FreqSummary(records = records) %>%
+#' FreqTable(census)$State
+#' FreqTable(census, 50, "d")
+#' FreqTable(census, 100, "d") #will throw an error, use FreqTable(census, 100, "l") instead
+FreqTable <- function(x, records = 10, as = "l") {
+  len <- length(x) * 2
+  s <-   x %>%
+    llply(
+      .fun = function(f) {
+        as_data_frame(head(sort(table(f), decreasing = T), records))
+      }
+    )
+
+  if (as == "l")
+    s
+  else
+  {
+    s %>%
     cbind(.[[1]]) %>%
     as_data_frame() %>%
     select(1:len)
+  }
 }
