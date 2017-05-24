ca7f979e19d14420689f2db9f2d9892813ebec0f
Prabhuram Venkatesan <prabhuram.v@gmail.com>
2017-05-24 08:53:55 -0400

    minor changes

--- R/themes.R
+++ R/themes.R
@@ -19,16 +19,16 @@ theme_cuckoo <- function(font_family = "Tahoma", base_size = 10, legend_position
   grid_minor_color <- "snow"
   strip_text_color <- "white"
   axis_line_color <- "gray"
-  update_geom_defaults(geom = "text", list(family = font_family))
-  update_geom_defaults(geom = "label", list(family = font_family, fill = "whitesmoke", size = 3))
-  update_geom_defaults(geom = "line", list(size = 2, colour = "Grey"))
+  update_geom_defaults(geom = "text", list(family = font_family, size = 1.5))
+  update_geom_defaults(geom = "label", list(family = font_family, fill = grid_major_color, size = 1.5))
+  update_geom_defaults(geom = "line", list(size = 2, colour = axis_line_color))
   #ggplot2::theme_bw(base_size = base_size, font_family = font_family) %+replace%
   theme(
     plot.margin =      unit(c(1,1,1,1),"mm")
     , plot.background = element_rect(fill=NA, color=NA, alp)
-    , plot.title = element_text(family="Arial Black", size=base_size, hjust = 0)
+    , plot.title = element_text(family="Arial Black", face = "bold", size=base_size, hjust = 0)
     , plot.subtitle=element_text(family=font_family, size=base_size)
-    , plot.caption=element_text(family=font_family, size=base_size-2)
+    , plot.caption=element_text(family=font_family, size=base_size-2, hjust = 0)
     , panel.grid.minor =  element_line(color = grid_minor_color)
     , panel.grid.major =  element_line(color = grid_major_color)
     , panel.border = element_rect(fill = NA, color = NA)
@@ -41,12 +41,12 @@ theme_cuckoo <- function(font_family = "Tahoma", base_size = 10, legend_position
     , legend.text = element_text(family=font_family, size=base_size-2)
     , strip.text.x = element_text(family=font_family, size=base_size, color = strip_text_color)
     , strip.text.y = element_text(family=font_family, size=base_size, color = strip_text_color)
-    , strip.background = element_rect(fill = "Gray", color =NA)
+    , strip.background = element_rect(fill = axis_line_color, color =NA)
     , axis.title = element_text(family=font_family, size=base_size)
     , axis.text = element_text(family=font_family, size=base_size)
     , axis.text.x = element_text(hjust = 1, vjust = 1)
-    , axis.ticks = element_line(color=NA)
-    , axis.line.x = element_line(color=axis_line_color, size = .3)
+    , axis.ticks = element_line(color=axis_line_color, size = 1)
+    , axis.line.x = element_line(color=axis_line_color, size = 1)
     , axis.line.y = element_line(color=NA)
     , ...
   )
